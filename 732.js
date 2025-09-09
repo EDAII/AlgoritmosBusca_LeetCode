1c5fb90cac1f2be59be1fc27c8a18d44fb3fbcb0
Lívia Rodrigues <80407134+Liviarodrigues1@users.noreply.github.com>
2025-09-08 21:57:57 -0300

    Update 732.js

--- 732.js
+++ 732.js
@@ -1,46 +1,47 @@
-class Node {
-  constructor() {
-    this.left = null;
-    this.right = null;
-    this.max = 0;
-    this.add = 0;
-  }
-}
-
 class MyCalendarThree {
   constructor() {
-    this.root = new Node();
-    this.L = 0;
-    this.R = 1e9; // domínio [0, 1e9]
+    this.keys = [];
+    this.delta = new Map();
+    this.peak = 0;
   }
 
   book(start, end) {
-    this.#update(this.root, this.L, this.R, start, end - 1, 1);
-    return this.root.max;
+    this.#addPoint(start, +1);
+    this.#addPoint(end, -1);
+    let run = 0, best = 0;
+    for (const t of this.keys) {
+      run += this.delta.get(t);
+      if (run > best) best = run;
+    }
+    this.peak = best;
+    return this.peak;
   }
 
-  #update(node, l, r, ql, qr, val) {
-    if (ql <= l && r <= qr) {
-      node.max += val;
-      node.add += val;
+  #addPoint(time, val) {
+    if (this.delta.has(time)) {
+      this.delta.set(time, this.delta.get(time) + val);
       return;
     }
-    const mid = (l + r) >> 1;
-    if (!node.left) node.left = new Node();
-    if (!node.right) node.right = new Node();
+    const idx = this.#lowerBound(this.keys, time);
+    this.keys.splice(idx, 0, time);
+    this.delta.set(time, val);
+  }
 
-    if (ql <= mid) this.#update(node.left, l, mid, ql, qr, val);
-    if (qr > mid)  this.#update(node.right, mid + 1, r, ql, qr, val);
-
-    node.max = node.add + Math.max(node.left.max, node.right.max);
+  #lowerBound(arr, x) {
+    let lo = 0, hi = arr.length;
+    while (lo < hi) {
+      const mid = (lo + hi) >> 1;
+      if (arr[mid] < x) lo = mid + 1;
+      else hi = mid;
+    }
+    return lo;
   }
 }
 
-// Exemplo de uso:
 const cal = new MyCalendarThree();
-console.log(cal.book(10, 20)); // 1
-console.log(cal.book(50, 60)); // 1
-console.log(cal.book(10, 40)); // 2
-console.log(cal.book(5, 15));  // 3
-console.log(cal.book(5, 10));  // 3
-console.log(cal.book(25, 55)); // 3
+console.log(cal.book(10, 20));
+console.log(cal.book(50, 60));
+console.log(cal.book(10, 40));
+console.log(cal.book(5, 15));
+console.log(cal.book(5, 10));
+console.log(cal.book(25, 55));
