40aeca532e71a10a979ba30c1d3ae480e42ee416
Lívia Rodrigues <80407134+Liviarodrigues1@users.noreply.github.com>
2025-09-08 21:59:40 -0300

    Update 493.js

--- 493.js
+++ 493.js
@@ -1,39 +1,35 @@
-function reversePairs(nums) {
-  const n = nums.length;
-  if (n < 2) return 0;
+function reversePairsBinarySearch(nums) {
+  const seen = [];
+  let count = 0;
 
-  const tmp = new Array(n);
-
-  function sort(l, r) {
-    if (l >= r) return 0;
-
-    const m = (l + r) >> 1;
-    let count = sort(l, m) + sort(m + 1, r);
-
-    let j = m + 1;
-    for (let i = l; i <= m; i++) {
-      while (j <= r && nums[i] > 2 * nums[j]) j++;
-      count += j - (m + 1);
+  function upperBound(arr, target) {
+    let l = 0, r = arr.length;
+    while (l < r) {
+      const m = (l + r) >> 1;
+      if (arr[m] <= target) l = m + 1;
+      else r = m;
     }
-
-    let i = l;
-    j = m + 1;
-    let k = l;
-
-    while (i <= m && j <= r) {
-      if (nums[i] <= nums[j]) tmp[k++] = nums[i++];
-      else tmp[k++] = nums[j++];
-    }
-    while (i <= m) tmp[k++] = nums[i++];
-    while (j <= r) tmp[k++] = nums[j++];
-
-    for (let t = l; t <= r; t++) nums[t] = tmp[t];
-
-    return count;
+    return l;
   }
 
-  return sort(0, n - 1);
+  function insertSorted(arr, val) {
+    let l = 0, r = arr.length;
+    while (l < r) {
+      const m = (l + r) >> 1;
+      if (arr[m] < val) l = m + 1;
+      else r = m;
+    }
+    arr.splice(l, 0, val);
+  }
+
+  for (const x of nums) {
+    const pos = upperBound(seen, 2 * x);
+    count += seen.length - pos;
+    insertSorted(seen, x);
+  }
+
+  return count;
 }
 
-console.log(reversePairs([1, 3, 2, 3, 1])); 
-console.log(reversePairs([2, 4, 3, 5, 1]));
+console.log(reversePairsBinarySearch([1, 3, 2, 3, 1])); 
+console.log(reversePairsBinarySearch([2, 4, 3, 5, 1]));
